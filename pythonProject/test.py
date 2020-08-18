250a3b398d6803005899413fc903ef11d8a9d29b
pbyvision <pbyvision@naver.com>
2020-08-18 23:01:30 +0900

    200818

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -2,22 +2,17 @@ from openpyxl import load_workbook
 from openpyxl.styles import Font, PatternFill
 import argparse
 
-#var = sys.argv[1]
-
 parser = argparse.ArgumentParser(description="주민번호 탐색기")
 parser.add_argument('--file', required=True, help='file name')
 parser.add_argument('--sheet', required=True, type=int,  help='sheet 위치번호')
 parser.add_argument('--col', required=True, help='검색할 Column 위치 ex) A B C')
 
 args = parser.parse_args()
+global c
+c=0
 
-
-#sheetnum = sys.argv[2]
-#searchColumn = sys.argv[3]
-
-
-def reg_num(cel_data):
-
+def reg_num(cel_data, num):
+    global c
     tmp = cel_data.split(" ")
     reg_result = 0
     for data in tmp:
@@ -27,7 +22,8 @@ def reg_num(cel_data):
             sex = data[6]
             if int(birth_day) < 32 and int(birth_month) < 13 and 5 > int(sex) > 0:
                 reg_result = 1
-                print(data)
+                print("라인 : " + str(num) + " 검출 주민번호 : " + data)
+                c += 1
     return reg_result
 
 
@@ -45,17 +41,21 @@ def search_columnlist(filename):
                           end_color='FFFF0000',
                           fill_type='solid')
     for i in search_column:
-        ch = reg_num(i.value)
         search_cnt += 1
-        if ch:
-            check_num_color = sheet.cell(row=search_cnt, column=3)
-            check_num_color.fill = redFill
+        if i.value:
+            ch = reg_num(i.value, search_cnt)
+            if ch:
+                check_num_color = sheet.cell(row=search_cnt, column=3)
+                check_num_color.fill = redFill
 
     wb.save("result.xlsx")
 
 
 def main():
+    global c
+    c = 0 #탐지 총 건수
     search_columnlist(args.file)
+    print("총 검출 수 : " + str(c))
 
 
 if __name__ == "__main__":
